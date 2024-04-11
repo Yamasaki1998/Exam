89df36c1942db0d3065b50cd7135952e36e22b8e
NekoSakana007 <166573178+NekoSakana007@users.noreply.github.com>
2024-04-11 11:43:15 +0900

    Update Student.java

--- java/bean/Student.java
+++ java/bean/Student.java
@@ -1,1 +1,29 @@
+package bean;
+
+public class Product implements java.io.Serializable {
+
+	private String NO;
+	private String NAME;
+	private int ENT_YEAR;
+
+	public String getNO() {
+		return NO;
+	}
+	public String getNAME() {
+		return NAME;
+	}
+	public int getENT_YEAR() {
+		return ENT_YEAR;
+	}
+
+	public void setNO(int NO) {
+		this.NO=NO;
+	}
+	public void setNAME(String NAME) {
+		this.NAME=NAME;
+	}
+	public void setENT_YEAR(int ENT_YEAR) {
+		this.ENT_YEAR=ENT_YEAR;
+	}
+}
 
