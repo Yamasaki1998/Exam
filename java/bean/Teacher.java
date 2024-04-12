17f1d4928983405146cba2d6fdcd4353942e79cf
NekoSakana007 <166573178+NekoSakana007@users.noreply.github.com>
2024-04-12 11:46:49 +0900

    Update Teacher.java

--- java/bean/Teacher.java
+++ java/bean/Teacher.java
@@ -3,27 +3,27 @@ package bean;
 
 public class Teacher implements java.io.Serializable {
 
-	private int id;
-	private String login;
+	private String id;
 	private String password;
+	private String name;
 
-	public int getId() {
+	public String getId() {
 		return id;
-	}
-	public String getLogin() {
-		return login;
 	}
 	public String getPassword() {
 		return password;
 	}
-	
-	public void setId(int id) {
-		this.id=id;
+	public String getName() {
+		return name;
 	}
-	public void setLogin(String login) {
-		this.login=login;
+	
+	public void setId(String id) {
+		this.id=id;
 	}
 	public void setPassword(String password) {
 		this.password=password;
+	}
+	public void setName(String name) {
+		this.name=name;
 	}
 }
