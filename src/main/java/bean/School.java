77b61065ceca80601015825fc833f8f160f28199
NekoSakana007 <166573178+NekoSakana007@users.noreply.github.com>
2024-04-16 10:42:56 +0900

    Create School.java

---
+++ src/main/java/bean/School.java
@@ -0,0 +1,21 @@
+package bean;
+
+public class school implements java.io.Serializable {
+
+	private String cd;
+	private String name;
+
+	public String getCd() {
+		return cd;
+	}
+	public String getName() {
+		return name;
+	}
+
+	public void setNo(String cd) {
+		this.cd=cd;
+	}
+	public void setName(String name) {
+		this.name=name;
+	}
+}
