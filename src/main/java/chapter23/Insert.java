2a8bfadff38c1c55b19f9836404a04d5b4391497
BGS0715 <166573502+BGS0715@users.noreply.github.com>
2024-04-17 11:34:41 +0900

    Create Insert.java

---
+++ src/main/java/chapter23/Insert.java
@@ -0,0 +1,44 @@
+package gakuseikanri;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import bean.Student;
+import dao.StudentDAO;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import tool.Page;
+
+@WebServlet(urlPatterns={"/gakuseikanri/insert"})
+public class Insert extends HttpServlet {
+
+	public void doPost (
+		HttpServletRequest request, HttpServletResponse response
+	) throws ServletException, IOException {
+		PrintWriter out=response.getWriter();
+		Page.header(out);
+		try {
+			int id=Integer.parseInt(request.getParameter("id"));
+			String name=request.getParameter("name");
+			String course=request.getParameter("course");
+
+			Student s=new Student();
+			s.setId(id);
+			s.setName(name);
+			s.setCourse(course);
+
+			StudentDAO dao=new StudentDAO();
+			int line=dao.insert(s);
+
+			if (line>0) {
+				out.println("追加に成功しました。");
+			}
+		} catch (Exception e) {
+			e.printStackTrace(out);
+		}
+		Page.footer(out);
+	}
+}
