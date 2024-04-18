90b67c4e6e5ed4ae3fb6bf04a0897d02d4390849
NekoSakana007 <166573178+NekoSakana007@users.noreply.github.com>
2024-04-18 10:20:43 +0900

    Update Insert.java

--- src/main/java/chapter23/Insert.java
+++ src/main/java/chapter23/Insert.java
@@ -1,44 +1,44 @@
-package chapter23;
+// package chapter23;
 
-import java.io.IOException;
-import java.io.PrintWriter;
+// import java.io.IOException;
+// import java.io.PrintWriter;
 
-import bean.Student;
-import dao.StudentDAO;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import tool.Page;
+// import bean.Student;
+// import dao.StudentDAO;
+// import jakarta.servlet.ServletException;
+// import jakarta.servlet.annotation.WebServlet;
+// import jakarta.servlet.http.HttpServlet;
+// import jakarta.servlet.http.HttpServletRequest;
+// import jakarta.servlet.http.HttpServletResponse;
+// import tool.Page;
 
-@WebServlet(urlPatterns={"/gakuseikanri/insert"})
-public class Insert extends HttpServlet {
+// @WebServlet(urlPatterns={"/gakuseikanri/insert"})
+// public class Insert extends HttpServlet {
 
-	public void doPost (
-		HttpServletRequest request, HttpServletResponse response
-	) throws ServletException, IOException {
-		PrintWriter out=response.getWriter();
-		Page.header(out);
-		try {
-			int id=Integer.parseInt(request.getParameter("id"));
-			String name=request.getParameter("name");
-			String course=request.getParameter("course");
+// 	public void doPost (
+// 		HttpServletRequest request, HttpServletResponse response
+// 	) throws ServletException, IOException {
+// 		PrintWriter out=response.getWriter();
+// 		Page.header(out);
+// 		try {
+// 			int id=Integer.parseInt(request.getParameter("id"));
+// 			String name=request.getParameter("name");
+// 			String course=request.getParameter("course");
 
-			Student s=new Student();
-			s.setId(id);
-			s.setName(name);
-			s.setCourse(course);
+// 			Student s=new Student();
+// 			s.setId(id);
+// 			s.setName(name);
+// 			s.setCourse(course);
 
-			StudentDAO dao=new StudentDAO();
-			int line=dao.insert(s);
+// 			StudentDAO dao=new StudentDAO();
+// 			int line=dao.insert(s);
 
-			if (line>0) {
-				out.println("追加に成功しました。");
-			}
-		} catch (Exception e) {
-			e.printStackTrace(out);
-		}
-		Page.footer(out);
-	}
-}
+// 			if (line>0) {
+// 				out.println("追加に成功しました。");
+// 			}
+// 		} catch (Exception e) {
+// 			e.printStackTrace(out);
+// 		}
+// 		Page.footer(out);
+// 	}
+// }
