7caae8a7e4ee8b79f3298b0db1336c7362e10b8b
NekoSakana007 <166573178+NekoSakana007@users.noreply.github.com>
2024-05-14 14:40:19 +0900

    Create SubjectListAction.java

---
+++ src/main/java/chapter23/SubjectListAction.java
@@ -0,0 +1,27 @@
+package chapter23;
+
+import java.util.List;
+
+import bean.Subject;
+import dao.SubjectDAO;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import tool.Action;
+
+public class SubjectListAction extends Action {
+	public String execute(
+		HttpServletRequest request, HttpServletResponse response
+	) throws Exception {
+
+		HttpSession session=request.getSession(); // セッションの開始
+
+		SubjectDAO dao=new SubjectDAO();
+		List<Subject> list=dao.search(""); // 科目一覧を取得 
+
+		session.setAttribute("list", list); // 科目一覧をlistという名前で保存
+
+		return "../chapter23/subjectlist.jsp"; // subjectList.jspに遷移
+
+	}
+}
