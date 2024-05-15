df5f4d0a751bcdc9cc23d0d376a371d823e098dd
NekoSakana007 <166573178+NekoSakana007@users.noreply.github.com>
2024-05-15 10:13:28 +0900

    Create StudentDeleteAction.java

---
+++ src/main/java/chapter23/StudentDeleteAction.java
@@ -0,0 +1,26 @@
+package chapter23;
+
+import java.util.List;
+
+import bean.Student;
+import dao.StudentDAO;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import tool.Action;
+
+public class StudentDeleteAction extends Action {
+    public String execute(
+        HttpServletRequest request, HttpServletResponse response
+    ) throws Exception {
+
+        String no = request.getParameter("no");
+        
+        StudentDAO dao = new StudentDAO();
+        dao.delete(no); // Assuming you have a delete method in your StudentDAO
+
+        List<Student> list = dao.search("");
+        request.setAttribute("list", list);
+
+        return "student_delete_ok.jsp";
+    }
+}
