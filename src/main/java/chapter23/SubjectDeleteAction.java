6cab6ee29587a859066ea319096f91a2e0c222cb
BGS0715 <166573502+BGS0715@users.noreply.github.com>
2024-05-15 14:10:23 +0900

    Create SubjectDeleteAction.java

---
+++ src/main/java/chapter23/SubjectDeleteAction.java
@@ -0,0 +1,26 @@
+package chapter23;
+
+import java.util.List;
+
+import bean.Subject;
+import dao.SubjectDAO;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import tool.Action;
+
+public class SubjectDeleteAction extends Action {
+    public String execute(
+        HttpServletRequest request, HttpServletResponse response
+    ) throws Exception {
+
+        String cd = request.getParameter("cd");
+        
+        SubjectDAO dao = new SubjectDAO();
+        dao.delete(cd); // Assuming you have a delete method in your StudentDAO
+
+        List<Subject> list = dao.search("");
+        request.setAttribute("list", list);
+
+        return "subject_delete.jsp";
+    }
+}
