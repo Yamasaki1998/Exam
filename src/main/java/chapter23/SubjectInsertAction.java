154f1fdeb89ead9f7fc3e3767a85cbd85f0bc56e
h_kono <h_kono@LAPTOP-8634DP9D>
2024-05-16 10:56:11 +0900

    5/16-6

--- src/main/java/chapter23/SubjectInsertAction.java
+++ src/main/java/chapter23/SubjectInsertAction.java
@@ -1,4 +1,3 @@
-
 package chapter23;
 
 import java.util.List;
@@ -10,24 +9,29 @@ import jakarta.servlet.http.HttpServletResponse;
 import tool.Action;
 
 public class SubjectInsertAction extends Action {
-	public String execute(
-		HttpServletRequest request, HttpServletResponse response
-	) throws Exception {
+    public String execute(
+        HttpServletRequest request, HttpServletResponse response
+    ) throws Exception {
 
-		String school_cd=request.getParameter("school_cd");
-		String cd=request.getParameter("cd");
-		String name=request.getParameter("name");
+        String school_cd = request.getParameter("school_cd");
+        String cd = request.getParameter("cd");
+        String name = request.getParameter("name");
 
-		Subject s=new Subject();
-		s.setSchool_cd(school_cd);
-		s.setCd(cd);
-		s.setName(name);
-		SubjectDAO dao=new SubjectDAO();
-		dao.insert(s);
+        Subject s = new Subject();
+        s.setSchool_cd(school_cd);
+        s.setCd(cd);
+        s.setName(name);
+        SubjectDAO dao = new SubjectDAO();
+        try {
+            dao.insert(s);
+        } catch (Exception e) {
+            // エラーが発生した場合に subject_insert_error.jsp に遷移
+            return "subject-insert-error.jsp";
+        }
 
-		List<Subject> list=dao.search("");
-		request.setAttribute("list", list);
+        List<Subject> list = dao.search("");
+        request.setAttribute("list", list);
 
-		return "subject_insert_ok.jsp";
-	}
+        return "subject_insert_ok.jsp";
+    }
 }
