f9fe46fa5d513905c5af3236dea2ff650050b5a7
BGS0715 <166573502+BGS0715@users.noreply.github.com>
2024-05-17 14:36:49 +0900

    Update Insert.java

--- src/main/java/chapter23/Insert.java
+++ src/main/java/chapter23/Insert.java
@@ -15,38 +15,39 @@ import tool.Page;
 @WebServlet(urlPatterns = { "/chapter23/insert" })
 public class Insert extends HttpServlet {
 
-	public void doPost(
-			HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		PrintWriter out = response.getWriter();
-		Page.header(out);
-		try {
-			String no = request.getParameter("no");
-			String name = request.getParameter("name");
-			String ent_year = request.getParameter("ent_year");
-			String class_num = request.getParameter("class_num");
-			Boolean isattend = Boolean.parseBoolean(request.getParameter("isattend"));
-			String school_cd = request.getParameter("school_cd");
+    public void doPost(
+            HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        Page.header(out);
+        try {
+            String no = request.getParameter("no");
+            String name = request.getParameter("name");
+            String ent_year = request.getParameter("ent_year");
+            String class_num = request.getParameter("class_num");
+            Boolean isattend = Boolean.parseBoolean(request.getParameter("isattend"));
+            String school_cd = request.getParameter("school_cd");
 
-			Student s = new Student();
-			s.setNo(no);
-			s.setName(name);
-			s.setEnt_year(ent_year);
-			s.setClass_num(class_num);
-			s.setIsattend(isattend);
-			s.setSchool_cd(school_cd);
+            Student s = new Student();
+            s.setNo(no);
+            s.setName(name);
+            s.setEnt_year(ent_year);
+            s.setClass_num(class_num);
+            s.setIsattend(isattend);
+            s.setSchool_cd(school_cd);
 
-			StudentDAO dao = new StudentDAO();
-			int line = dao.insert(s);
+            StudentDAO dao = new StudentDAO();
+            int line = dao.insert(s);
 
-			if (line > 0) {
-				out.println("追加に成功しました。<br><br><a href=\"../chapter25/menu.jsp\">メニューに戻る</a> <a href=\"../chapter23/insert.jsp\">学生の追加</a>\n"
-						+ "");
-			} else {
-				response.sendRedirect("../chapter24/insert-error.jsp");
-			}
-		} catch (Exception e) {
-			response.sendRedirect("../chapter24/insert-error.jsp");
-		}
-		Page.footer(out);
-	}
+            if (line > 0) {
+            	 response.sendRedirect("../chapter24/insert_out.jsp");
+            } else {
+                // エラーが発生した場合に同じページにリダイレクト
+                response.sendRedirect("../chapter24/insert-error.jsp");
+            }
+        } catch (Exception e) {
+            // エラーが発生した場合に同じページにリダイレクト
+            response.sendRedirect("../chapter24/insert-error.jsp");
+        }
+        Page.footer(out);
+    }
 }
